aaede3bc2ae97b51e640d13eb87a2732d0b8f723
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-10-18 14:02:04 +0200

    Example of iframe widget integration

--- app/assets/javascripts/mashetes/src/iframe.js
+++ app/assets/javascripts/mashetes/src/iframe.js
@@ -8,13 +8,27 @@ portal.MashetesStore = portal.MashetesStore || {};
             return {
                 displayedTitle: this.props.title,
                 displayedUrl: this.props.url,
-                displayedHeight: this.props.height || 500
+                displayedHeight: this.props.height || '500px',
+                displayedWidth: this.props.width || "100%",
+                displayedAllowTransparency: this.props.allowTransparency || true,
+                displayedClass: this.props.class || 'pushTop',
+                displayedFrameborder: this.props.frameborder || "0"
             };
         },
         render: function() {
             return (
                 <portal.Mashetes.Mashete title={this.state.displayedTitle} config={this.props}>
-                    <iframe className="pushTop" src={this.state.displayedUrl} width="100%" height={this.state.displayedHeight}></iframe>
+                    <div className="centeredText">
+                        <iframe
+                            className={this.state.displayedClass}
+                            src={this.state.displayedUrl}
+                            title={this.state.displayedTitle}
+                            allowTransparency={this.state.displayedAllowTransparency}
+                            frameborder={this.state.displayedFrameborder}
+                            width={this.state.displayedWidth}
+                            height={this.state.displayedHeight}
+                        ></iframe>
+                    </div>
                 </portal.Mashetes.Mashete>
             );
         }
