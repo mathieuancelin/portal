2808f6dfa14323aadcaed28cf7970f4ca30f0c1c
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-10-29 10:30:27 +0100

    Cleanup architecture

--- app/assets/javascripts/mashetes/build/todo.js
+++ app/assets/javascripts/mashetes/build/todo.js
@@ -2,127 +2,8 @@
 
 var portal = portal || {};
 portal.MashetesStore = portal.MashetesStore || {};
-(function(exports) {
 
-    var SANDBOX = 'com.foo.bar.TodoMashete';
-    var DOC_TYPE = 'TodoMashete.Task';
-    var repository = portal.Repository.of(SANDBOX);
-
-    var TaskItem =  React.createClass({displayName: 'TaskItem',
-        getInitialState: function () {
-            return {
-                done: this.props.task.done
-            };
-        },
-        change: function() {
-            this.setState({done: !this.state.done}, function() {
-                this.props.updateDone(this.props.task._id, this.state.done);
-            }.bind(this));
-        },
-        render: function () {
-            var cx = React.addons.classSet;
-            var classes = cx({
-                'label': true,
-                'label-success': this.state.done,
-                'label-default': !this.state.done
-            });
-            return (
-                React.createElement("li", {className: "list-group-item"}, 
-                    React.createElement("div", {className: "row"}, 
-                        React.createElement("div", {className: "col-md-10"}, 
-                            this.props.task.name
-                        ), 
-                        React.createElement("div", {className: "col-md-2"}, 
-                            React.createElement("span", {onClick: this.change, className: classes}, "Done")
-                        )
-                    )
-                )
-            );
-        }
-    });
-
-    exports.TodoMashete = React.createClass({displayName: 'TodoMashete',
-        getInitialState: function() {
-            return {
-                tasks: [],
-                taskName: ''
-            };
-        },
-        componentDidMount: function() {
-            this.reloadList();
-        },
-        updateName: function(e) {
-            this.setState({taskName: e.target.value});
-        },
-        reloadList: function() {
-            repository.search({ docType: DOC_TYPE }).then(function(data) {
-                this.setState({tasks: data || []});
-            }.bind(this));
-        },
-        save: function() {
-            var task = {
-                name: this.state.taskName,
-                done: false,
-                docType: DOC_TYPE
-            };
-            var fakeTasks = this.state.tasks;
-            fakeTasks.push(task);
-            this.setState({tasks: fakeTasks}); // for user responsiveness
-            repository.save(task).then(function() {
-                this.reloadList();
-                this.setState({taskName: ''});
-            }.bind(this));
-        },
-        deleteAll: function() {
-            repository.removeSelection({
-                docType: DOC_TYPE,
-                done: true
-            }).then(function() {
-                this.reloadList();
-            }.bind(this));
-        },
-        render: function() {
-            var _this = this;
-            var displayedTasks = _.map(this.state.tasks, function(item) {
-                function change(id, done) {
-                    repository.save({
-                        docType: DOC_TYPE,
-                        _id: id,
-                        done: done
-                    }).then(function() {
-                        _this.reloadList();
-                    }.bind(_this));
-                }
-                return (React.createElement(TaskItem, {key: item._id, task: item, updateDone: change}));
-            });
-            return (
-                React.createElement(portal.Mashetes.Mashete, {title: "Todo list", config: this.props}, 
-                    React.createElement("h3", null, "Todo List"), 
-                    React.createElement("div", null, 
-                        React.createElement("div", {className: "row"}, 
-                            React.createElement("form", {role: "form"}, 
-                                React.createElement("div", {className: "form-group col-md-10"}, 
-                                    React.createElement("input", {placeholder: "What do you have to do ?", type: "text", className: "form-control", value: this.state.taskName, onChange: this.updateName})
-                                ), 
-                                React.createElement("div", {className: "form-group"}, 
-                                    React.createElement("div", {className: "btn-group"}, 
-                                        React.createElement("button", {type: "button", onClick: this.save, className: "btn btn-success"}, React.createElement("span", {className: "glyphicon glyphicon-floppy-saved"})), 
-                                        React.createElement("button", {type: "button", onClick: this.deleteAll, className: "btn btn-danger"}, React.createElement("span", {className: "glyphicon glyphicon-trash"}))
-                                    )
-                                )
-                            )
-                        )
-                    ), 
-                    React.createElement("ul", {className: "list-group"}, 
-                    displayedTasks
-                    )
-                )
-            );
-        }
-    });
-})(portal.MashetesStore);
-
-
+// Nicer version of the Todo App using Flux like architecture (http://facebook.github.io/flux/)
 (function(exports) {
 
     var Constants = {
@@ -246,27 +127,23 @@ portal.MashetesStore = portal.MashetesStore || {};
         }
     });
 
-    var TodoApp = React.createClass({displayName: 'TodoApp',
+    var NewTask = React.createClass({displayName: 'NewTask',
         getInitialState: function() {
             return {
-                tasks: [],
                 taskName: ''
             };
         },
+        clearTaskName: function() {
+            console.log('Cleanup text');
+            this.setState({
+                taskName: ''
+            });
+        },
         componentDidMount: function() {
-            TaskStore.init();
-            Dispatcher.on(Constants.TASKS_CHANGED, function() {
-                console.log('Re-render tasks ');
-                this.setState({
-                    tasks: TaskStore.getAllTasks()
-                });
-            }.bind(this));
-            Dispatcher.on(Constants.TASKS_ADDED, function() {
-                console.log('Cleanup text ');
-                this.setState({
-                    taskName: ''
-                });
-            }.bind(this));
+            Dispatcher.on(Constants.TASKS_ADDED, this.clearTaskName);
+        },
+        componentWillUnmount: function() {
+            Dispatcher.off(Constants.TASKS_ADDED, this.clearTaskName);
         },
         updateName: function(e) {
             this.setState({taskName: e.target.value});
@@ -277,6 +154,52 @@ portal.MashetesStore = portal.MashetesStore || {};
         deleteAll: function() {
             TaskActions.deleteDone();
         },
+        keyPress: function(e) {
+            if (e.key === 'Enter') {
+                this.save();
+                e.preventDefault();
+            }
+        },
+        render: function() {
+            return (
+                React.createElement("div", null, 
+                    React.createElement("div", {className: "row"}, 
+                        React.createElement("form", {role: "form"}, 
+                            React.createElement("div", {className: "form-group col-md-10"}, 
+                                React.createElement("input", {placeholder: "What do you have to do ?", type: "text", className: "form-control", value: this.state.taskName, onChange: this.updateName, onKeyPress: this.keyPress})
+                            ), 
+                            React.createElement("div", {className: "form-group"}, 
+                                React.createElement("div", {className: "btn-group"}, 
+                                    React.createElement("button", {type: "button", onClick: this.save, className: "btn btn-success"}, React.createElement("span", {className: "glyphicon glyphicon-floppy-saved"})), 
+                                    React.createElement("button", {type: "button", onClick: this.deleteAll, className: "btn btn-danger"}, React.createElement("span", {className: "glyphicon glyphicon-trash"}))
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    });
+
+    var TodoApp = React.createClass({displayName: 'TodoApp',
+        getInitialState: function() {
+            return {
+                tasks: []
+            };
+        },
+        reloadTasks: function() {
+            console.log('Re-render tasks');
+            this.setState({
+                tasks: TaskStore.getAllTasks()
+            });
+        },
+        componentDidMount: function() {
+            TaskStore.init();
+            Dispatcher.on(Constants.TASKS_CHANGED, this.reloadTasks);
+        },
+        componentWillUnmount: function() {
+            Dispatcher.off(Constants.TASKS_CHANGED, this.reloadTasks);
+        },
         render: function() {
             var displayedTasks = _.map(this.state.tasks, function(item) {
                 return (React.createElement(TaskItem, {key: item._id, task: item}));
@@ -284,21 +207,7 @@ portal.MashetesStore = portal.MashetesStore || {};
             return (
                 React.createElement(portal.Mashetes.Mashete, {title: "Todo list", config: this.props}, 
                     React.createElement("h3", null, "Todo List"), 
-                    React.createElement("div", null, 
-                        React.createElement("div", {className: "row"}, 
-                            React.createElement("form", {role: "form"}, 
-                                React.createElement("div", {className: "form-group col-md-10"}, 
-                                    React.createElement("input", {placeholder: "What do you have to do ?", type: "text", className: "form-control", value: this.state.taskName, onChange: this.updateName})
-                                ), 
-                                React.createElement("div", {className: "form-group"}, 
-                                    React.createElement("div", {className: "btn-group"}, 
-                                        React.createElement("button", {type: "button", onClick: this.save, className: "btn btn-success"}, React.createElement("span", {className: "glyphicon glyphicon-floppy-saved"})), 
-                                        React.createElement("button", {type: "button", onClick: this.deleteAll, className: "btn btn-danger"}, React.createElement("span", {className: "glyphicon glyphicon-trash"}))
-                                    )
-                                )
-                            )
-                        )
-                    ), 
+                    React.createElement(NewTask, null), 
                     React.createElement("ul", {className: "list-group"}, 
                     displayedTasks
                     )
--- app/assets/javascripts/mashetes/src/todo.js
+++ app/assets/javascripts/mashetes/src/todo.js
@@ -2,127 +2,8 @@
 
 var portal = portal || {};
 portal.MashetesStore = portal.MashetesStore || {};
-(function(exports) {
 
-    var SANDBOX = 'com.foo.bar.TodoMashete';
-    var DOC_TYPE = 'TodoMashete.Task';
-    var repository = portal.Repository.of(SANDBOX);
-
-    var TaskItem =  React.createClass({
-        getInitialState: function () {
-            return {
-                done: this.props.task.done
-            };
-        },
-        change: function() {
-            this.setState({done: !this.state.done}, function() {
-                this.props.updateDone(this.props.task._id, this.state.done);
-            }.bind(this));
-        },
-        render: function () {
-            var cx = React.addons.classSet;
-            var classes = cx({
-                'label': true,
-                'label-success': this.state.done,
-                'label-default': !this.state.done
-            });
-            return (
-                <li className="list-group-item">
-                    <div className="row">
-                        <div className="col-md-10">
-                            {this.props.task.name}
-                        </div>
-                        <div className="col-md-2">
-                            <span onClick={this.change} className={classes}>Done</span>
-                        </div>
-                    </div>
-                </li>
-            );
-        }
-    });
-
-    exports.TodoMashete = React.createClass({
-        getInitialState: function() {
-            return {
-                tasks: [],
-                taskName: ''
-            };
-        },
-        componentDidMount: function() {
-            this.reloadList();
-        },
-        updateName: function(e) {
-            this.setState({taskName: e.target.value});
-        },
-        reloadList: function() {
-            repository.search({ docType: DOC_TYPE }).then(function(data) {
-                this.setState({tasks: data || []});
-            }.bind(this));
-        },
-        save: function() {
-            var task = {
-                name: this.state.taskName,
-                done: false,
-                docType: DOC_TYPE
-            };
-            var fakeTasks = this.state.tasks;
-            fakeTasks.push(task);
-            this.setState({tasks: fakeTasks}); // for user responsiveness
-            repository.save(task).then(function() {
-                this.reloadList();
-                this.setState({taskName: ''});
-            }.bind(this));
-        },
-        deleteAll: function() {
-            repository.removeSelection({
-                docType: DOC_TYPE,
-                done: true
-            }).then(function() {
-                this.reloadList();
-            }.bind(this));
-        },
-        render: function() {
-            var _this = this;
-            var displayedTasks = _.map(this.state.tasks, function(item) {
-                function change(id, done) {
-                    repository.save({
-                        docType: DOC_TYPE,
-                        _id: id,
-                        done: done
-                    }).then(function() {
-                        _this.reloadList();
-                    }.bind(_this));
-                }
-                return (<TaskItem key={item._id} task={item} updateDone={change}/>);
-            });
-            return (
-                <portal.Mashetes.Mashete title="Todo list" config={this.props}>
-                    <h3>Todo List</h3>
-                    <div>
-                        <div className="row">
-                            <form role="form">
-                                <div className="form-group col-md-10">
-                                    <input placeholder="What do you have to do ?" type="text" className="form-control" value={this.state.taskName} onChange={this.updateName}/>
-                                </div>
-                                <div className="form-group">
-                                    <div className="btn-group">
-                                        <button type="button" onClick={this.save} className="btn btn-success"><span className="glyphicon glyphicon-floppy-saved"></span></button>
-                                        <button type="button" onClick={this.deleteAll} className="btn btn-danger"><span className="glyphicon glyphicon-trash"></span></button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <ul className="list-group">
-                    {displayedTasks}
-                    </ul>
-                </portal.Mashetes.Mashete>
-            );
-        }
-    });
-})(portal.MashetesStore);
-
-
+// Nicer version of the Todo App using Flux like architecture (http://facebook.github.io/flux/)
 (function(exports) {
 
     var Constants = {
@@ -246,27 +127,23 @@ portal.MashetesStore = portal.MashetesStore || {};
         }
     });
 
-    var TodoApp = React.createClass({
+    var NewTask = React.createClass({
         getInitialState: function() {
             return {
-                tasks: [],
                 taskName: ''
             };
         },
+        clearTaskName: function() {
+            console.log('Cleanup text');
+            this.setState({
+                taskName: ''
+            });
+        },
         componentDidMount: function() {
-            TaskStore.init();
-            Dispatcher.on(Constants.TASKS_CHANGED, function() {
-                console.log('Re-render tasks ');
-                this.setState({
-                    tasks: TaskStore.getAllTasks()
-                });
-            }.bind(this));
-            Dispatcher.on(Constants.TASKS_ADDED, function() {
-                console.log('Cleanup text ');
-                this.setState({
-                    taskName: ''
-                });
-            }.bind(this));
+            Dispatcher.on(Constants.TASKS_ADDED, this.clearTaskName);
+        },
+        componentWillUnmount: function() {
+            Dispatcher.off(Constants.TASKS_ADDED, this.clearTaskName);
         },
         updateName: function(e) {
             this.setState({taskName: e.target.value});
@@ -277,6 +154,52 @@ portal.MashetesStore = portal.MashetesStore || {};
         deleteAll: function() {
             TaskActions.deleteDone();
         },
+        keyPress: function(e) {
+            if (e.key === 'Enter') {
+                this.save();
+                e.preventDefault();
+            }
+        },
+        render: function() {
+            return (
+                <div>
+                    <div className="row">
+                        <form role="form">
+                            <div className="form-group col-md-10">
+                                <input placeholder="What do you have to do ?" type="text" className="form-control" value={this.state.taskName} onChange={this.updateName} onKeyPress={this.keyPress}/>
+                            </div>
+                            <div className="form-group">
+                                <div className="btn-group">
+                                    <button type="button" onClick={this.save} className="btn btn-success"><span className="glyphicon glyphicon-floppy-saved"></span></button>
+                                    <button type="button" onClick={this.deleteAll} className="btn btn-danger"><span className="glyphicon glyphicon-trash"></span></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            );
+        }
+    });
+
+    var TodoApp = React.createClass({
+        getInitialState: function() {
+            return {
+                tasks: []
+            };
+        },
+        reloadTasks: function() {
+            console.log('Re-render tasks');
+            this.setState({
+                tasks: TaskStore.getAllTasks()
+            });
+        },
+        componentDidMount: function() {
+            TaskStore.init();
+            Dispatcher.on(Constants.TASKS_CHANGED, this.reloadTasks);
+        },
+        componentWillUnmount: function() {
+            Dispatcher.off(Constants.TASKS_CHANGED, this.reloadTasks);
+        },
         render: function() {
             var displayedTasks = _.map(this.state.tasks, function(item) {
                 return (<TaskItem key={item._id} task={item}/>);
@@ -284,21 +207,7 @@ portal.MashetesStore = portal.MashetesStore || {};
             return (
                 <portal.Mashetes.Mashete title="Todo list" config={this.props}>
                     <h3>Todo List</h3>
-                    <div>
-                        <div className="row">
-                            <form role="form">
-                                <div className="form-group col-md-10">
-                                    <input placeholder="What do you have to do ?" type="text" className="form-control" value={this.state.taskName} onChange={this.updateName}/>
-                                </div>
-                                <div className="form-group">
-                                    <div className="btn-group">
-                                        <button type="button" onClick={this.save} className="btn btn-success"><span className="glyphicon glyphicon-floppy-saved"></span></button>
-                                        <button type="button" onClick={this.deleteAll} className="btn btn-danger"><span className="glyphicon glyphicon-trash"></span></button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                    <NewTask />
                     <ul className="list-group">
                     {displayedTasks}
                     </ul>
